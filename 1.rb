e3b4a0c4d4377e4cb9421c13ad1eaa6e87be9dbb
Nickollay <38377768+Nickollay@users.noreply.github.com>
2020-03-06 13:12:03 +0300

    Update 1.rb

--- 1.rb
+++ 1.rb
@@ -1,6 +1,18 @@
-months = {jan: 31, feb: 28, mar: 31, apr: 30, may: 31, jun: 30, jul: 31, aug: 31, sep: 30, oct: 31, nov: 30, dec: 31}
+months = {
+  jan: 31,
+  feb: 28,
+  mar: 31,
+  apr: 30,
+  may: 31,
+  jun: 30,
+  jul: 31,
+  aug: 31,
+  sep: 30,
+  oct: 31,
+  nov: 30,
+  dec: 31
+}
 months.each do |month, days|
-  if days == 30
-    puts month
+  puts month if days == 30
   end
 end
